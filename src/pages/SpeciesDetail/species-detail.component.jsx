ed98c393e150456963ae89aa0491b8dc4ca34fc8
RezaAlpadizami <alpdizami94@gmail.com>
2023-10-20 14:31:33 +0700

    fix key content page species

--- src/pages/SpeciesDetail/species-detail.component.jsx
+++ src/pages/SpeciesDetail/species-detail.component.jsx
@@ -59,13 +59,13 @@ const SpeciesDetail = () => {
               />
             </Col>
             <Col span={6}>
-              <CardDetail title={"Hair Color"} content={data?.hair_color} />
+              <CardDetail title={"Hair Color"} content={data?.hair_colors} />
             </Col>
             <Col span={6}>
-              <CardDetail title={"Skin Color"} content={data?.skin_color} />
+              <CardDetail title={"Skin Color"} content={data?.skin_colors} />
             </Col>
             <Col span={6}>
-              <CardDetail title={"Eye Color"} content={data?.eye_color} />
+              <CardDetail title={"Eye Color"} content={data?.eye_colors} />
             </Col>
             <Col span={6}>
               <CardDetail
